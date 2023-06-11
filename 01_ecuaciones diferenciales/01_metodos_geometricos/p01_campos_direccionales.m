651bbbea83df13c3537de847c3da9837c510dda8
alvarobatrez <alvarobatrez27@gmail.com>
2023-06-11 10:20:31 -0600

    new commit

--- 01_ecuaciones diferenciales/01_metodos_geometricos/p01_campos_direccionales.m
+++ 01_ecuaciones diferenciales/01_metodos_geometricos/p01_campos_direccionales.m
@@ -1,10 +1,12 @@
 close all; clear, clc
 
-[x, y] = meshgrid(-4:0.5:4, -4:0.5:4);
+[x, y] = meshgrid(-2:0.2:2, -2:0.2:2);
 
-s = y.^2 - x.^2;
+s = 3*x - y;
 
 L = sqrt(1 + s.^2); % L es para reescalar la magnitud de las flechas
 
 quiver(x,y, 1./L, s./L, 0.5) % 0.5 se refiere al tamaño de las flechas
+
+axis([-2.2 2.2 -2.2 2.2])
 xlabel('x'), ylabel('y'), title('Campo de dirección')
