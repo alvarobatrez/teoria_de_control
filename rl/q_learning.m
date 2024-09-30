fda5223d99230344cdb6bb71e0e2eb102e01ad83
alvarobatrez <alvarobatrez27@gmail.com>
2024-09-29 19:03:41 -0600

    q learning

---
+++ rl/q_learning.m
@@ -0,0 +1,70 @@
+close all; clear, clc
+
+R = create_maze();
+actions = [-1 0; 0 1; 1 0; 0 -1];
+
+[m, n] = size(R);
+num_actions = length(actions);
+
+alpha = 0.1;
+gamma = 0.99;
+epsilon = 1;
+decay = 0.99;
+num_episodes = 1000;
+
+Q = zeros(m, n, num_actions);
+
+start_position = [1 1];
+[goal_row, goal_col] = find(R==0);
+
+for episode = 1 : num_episodes
+    epsilon = max(0.1, decay*epsilon);
+    state = start_position;
+    action = egreedy_action(epsilon, Q, state, num_actions);
+
+    while ~isequal(state, [goal_row, goal_col])
+
+        [next_state, reward] = step(R, state, action, actions, m, n);
+        next_action = egreedy_action(epsilon, Q, next_state, num_actions);
+        Q(state(1), state(2), action) = Q(state(1), state(2), action) + alpha * (reward + gamma * Q(next_state(1), next_state(2), next_action) - Q(state(1), state(2), action));
+        state = next_state;
+        action = next_action;
+    end
+end
+
+[~, policy] = max(Q, [], 3);
+policy(R==1) = 0;
+policy(R==0) = 0;
+
+disp('Acciones: 1=arriba, 2=derecha, 3=abajo, 4=izquierda')
+disp('Politica Optima')
+disp(policy)
+
+plot_q_values(Q)
+
+[row, col] = find(R == 0);
+draw_maze(R, start_position, policy, [row col])
+
+function action = egreedy_action(epsilon, Q, state, num_actions)
+    if rand > epsilon
+        [~, action] = max(Q(state(1), state(2), :));
+    else
+        action = randi(num_actions);
+    end
+end
+
+function [next_state, reward] = step(R, state, action, actions, m, n)
+    i = state(1);
+    j = state(2);
+
+    new_i = i + actions(action, 1);
+    new_j = j + actions(action, 2);
+
+    if new_i >= 1 && new_i <= m && new_j >=1 && new_j <= n && R(new_i, new_j) ~= 1
+        i = new_i;
+        j = new_j;
+    end
+
+    next_state = [i j];
+    reward = R(i, j);
+end
