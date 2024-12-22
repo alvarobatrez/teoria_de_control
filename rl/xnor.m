e1a835e0e570094011dc3bbf5c15cbc21767c119
Álvaro Bátrez <alvarobatrez27@gmail.com>
2024-12-21 22:41:56 -0600

    w2 tranpuesta

--- rl/xnor.m
+++ rl/xnor.m
@@ -7,7 +7,7 @@ X = [1 0 0; 1 0 1; 1 1 0; 1 1 1];
 Y = [1; 0; 0; 1];
 
 w1 = [0.3 -0.1 0.2; 0.1 0.2 0];
-w2 = [-0.3; 0.1; -0.2];
+w2 = [-0.3 0.1 -0.2];
 
 sig = @(x) 1 ./ (1 + exp(-x));
 dsig = @(x) exp(-x) ./ (1 + exp(-x)).^2;
@@ -19,16 +19,20 @@ for epoch = 1 : epochs
     for i = 1 : length(X)
         x = X(i,:)';
         y = Y(i);
-
+        
+        % Propagacion hacia adelante
         h = [1; sig(w1 * x)];
-        o = sig(w2' * h);
-
+        o = sig(w2 * h);
+    
+        % Calculo del error
         e = e + 0.5 * (y - o).^2;
 
-        delta_o = (o - y) * dsig(w2' * h);
-        delta_h = dsig(w1 * x) .* (w2(2:end) * delta_o);
+        % Retropropagacion
+        delta_o = (o - y) * dsig(w2 * h);
+        delta_h = dsig(w1 * x) .* (w2(2:end)' * delta_o);
 
-        w2 = w2 - eta * delta_o * h;
+        % Actualización de los pesos
+        w2 = w2 - eta * delta_o * h';
         w1 = w1 - eta * delta_h * x';
     end
 
@@ -46,7 +50,7 @@ for i = 1 : length(X)
     y = Y(i);
 
     h = [1; sig(w1 * x')];
-    o = sig(w2' * h);
+    o = sig(w2 * h);
 
     fprintf('Entrada: [%d %d], Salida: %.1f\n', X(i,1), X(i,2), o)
 end
