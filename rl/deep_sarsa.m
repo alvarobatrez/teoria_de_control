0d60933eb9b55d85851af087e468c2f55f1c7d5b
alvarobatrez <alvarobatrez27@gmail.com>
2025-02-22 23:52:16 -0600

    laberinto

--- rl/deep_sarsa.m
+++ rl/deep_sarsa.m
@@ -1,6 +1,6 @@
 close all; clear, clc
 
-M = create_maze_small();
+M = create_maze();
 actions = [-1 0; 0 1; 1 0; 0 -1];
 
 start_position = [1 2];
@@ -9,17 +9,19 @@ start_position = [1 2];
 [m, n] = size(M);
 num_actions = length(actions);
 
-gamma = 0.9;
+tau = 0.005;
+gamma = 0.99;
 epsilon = 1;
-decay = 0.99;
-num_episodes = 1000;
+decay = 0.995;
+num_episodes = 2500;
+max_steps = 1e5;
 
-buffer_capacity = 1e2;
-batch_size = 8;
+buffer_capacity = 1e6;
+batch_size = 32;
 buffer = ExperienceReplay(buffer_capacity);
 
 num_inputs = 2;
-layers = {{8, 'relu'} {4, 'linear'}};
+layers = {{128, 'relu'} {32, 'relu'} {8, 'relu'} {4, 'linear'}};
 
 learning_rate = 0.001;
 optimizer = 'adam';
@@ -32,12 +34,17 @@ target_network = NeuralNetwork(num_inputs, layers);
 target_network = target_network.compile(learning_rate, optimizer, loss_function);
 target_network = copy_weights(q_network, target_network);
 
+total_loss = zeros(num_episodes, 1);
+
 for episode = 1 : num_episodes
     epsilon = max(0.1, decay*epsilon);
     state = start_position;
+    steps = 0;
     loss = 0;
+    n = 0;
     
-    while ~isequal(state, [goal_row goal_col])
+    while ~isequal(state, [goal_row goal_col]) && steps < max_steps
+        steps = steps + 1;
         action = egreedy_action(epsilon, q_network, state, num_actions);
         [next_state, reward, done] = step(M, state, action, actions, m, n);
 
@@ -52,21 +59,30 @@ for episode = 1 : num_episodes
             target_b = reward_b + (1 - done_b) * gamma .* next_q_b;
 
             q_b = gather_q(q_network, state_b, action_b, batch_size);
-            loss = loss + mean((target_b - q_b).^2);
             q_network = backpropagation(q_network, batch_size, state_b, target_b, action_b);
+
+            target_network = update_target_network(q_network, target_network, tau);
+            
+            n = n + 1;
+            loss = loss + 1 / n * (mean((target_b - q_b).^2) - loss);
         end        
         
         state = next_state;
     end
 
-    if mod(episode, 10) == 0
-        target_network = copy_weights(q_network, target_network);
-    end
+    % if mod(episode, 10) == 0
+    %     target_network = copy_weights(q_network, target_network);
+    % end
+
+    total_loss(episode) = loss;
     
-    fprintf('Episodio: %d, Pérdida: %.4f\n', episode, loss)
+    fprintf('Episodio: %d, Pasos: %d, Pérdida: %.4f\n', episode, steps, loss)
 end
 
 policy = create_policy(q_network, M);
+
+plot(1:num_episodes, total_loss), grid on
+title('Deep Sarsa'), xlabel('Épocas'), ylabel('Error (MSE)')
 
 draw_maze(M, start_position, policy, [goal_row goal_col])
 
@@ -129,6 +145,12 @@ end
 function model = backpropagation(model, batch_size, state, target, action)
     grad = compute_loss_gradients(model, batch_size, state, target, action);
     model = model.update_weights(grad);
+end
+
+function model_target = update_target_network(model, model_target, tau)
+    for i = 1 : model.num_layers
+        model_target.layers{i}.weights = tau * model.layers{i}.weights + (1 - tau) * model_target.layers{i}.weights;
+    end
 end
 
 function policy = create_policy(model, M)
